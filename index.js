0c32e4aaf4a276c80307a3647b3a2481b65d4a57
testted <31362216+hugochen1020@users.noreply.github.com>
2017-11-07 04:24:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ var express = require('express');
 var bot = linebot({
   channelId: 1544693039,
   channelSecret: 0f322fbde7d4b70600a440b56d493add,
-  channelAccessToken: a91yhLAqUKTiPu5cKIFKSoeCrrf6
-  					  8rXLcZW7AwMXM3shqTAjrr/MXlCgMKfWKRQuG3hvsQQdKSaL
-    				  5kTwEL8PJGfi9JomJNS9xppVix7sCvlaKrWt9HJnIG5Rr9fK
-                      xDHih2rbXCTIVuoCJeV8HWGmpgdB04t89/1O/w1cDnyilFU=
+  channelAccessToken: a91yhLAqUKTiPu5cKIFKSoeCrrf68rXLcZW7AwMXM3shqTAjrr/MXlCgMKfWKRQuG3hvsQQdKSaL5kTwEL8PJGfi9JomJNS9xppVix7sCvlaKrWt9HJnIG5Rr9fKxDHih2rbXCTIVuoCJeV8HWGmpgdB04t89/1O/w1cDnyilFU=
 });                      
 
 bot.on('message', function(event) {
